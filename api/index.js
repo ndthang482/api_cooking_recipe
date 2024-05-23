09f3372e817f3104f2f7fe10da2c2aaf5b4e05ff
ndthang482 <thangthontien2k@gmail.com>
2024-05-23 13:57:01 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin123',
+  database: 'recipe_db'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the MySQL database.');
+  }
+});
+
+app.get('/recipes', (req, res) => {
+  const query = 'SELECT * FROM recipes';
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM recipes WHERE id = ?';
+  db.query(query, [id], (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+}); 
