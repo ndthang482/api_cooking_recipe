dfc3764245f99c6ffa590adee293578ccce2e73c
ndthang482 <thangthontien2k@gmail.com>
2024-05-24 21:53:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ app.get('/api/recipes/:id', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
